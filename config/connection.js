8ef5f7846b9c1c925e699d60207e97a67e118dce
Mr. Galvan <112872430+Juang1908@users.noreply.github.com>
2023-02-09 19:15:05 -0600

    updated product/seeds by category/id

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 const sequelize = new Sequelize(
-    'listup_db',
-    'root',
-    'mypassword',
-    {
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        port: 3306
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306,
+  }
 );
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 
 const sess = {
@@ -18,10 +18,11 @@ const sess = {
     db: sequelize,
   }),
 };
-
+const app = express();
+const PORT= process.env.PORT || 3001;
 app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
